70bdb7be66233bf030331a9a285019dff53d5c6d
john edwin lopez marulanda <61915309+johnedwinlopezm@users.noreply.github.com>
2023-12-26 06:40:15 -0800

    vercel json

--- src/main.ts
+++ src/main.ts
@@ -8,7 +8,7 @@ import cookieParser from 'cookie-parser';
 import { routerApi } from './routes';
 
 const app = express();
-const port = 3000;
+const port = process.env.PORT || 3000;
 
 
 app.use(express.json());
